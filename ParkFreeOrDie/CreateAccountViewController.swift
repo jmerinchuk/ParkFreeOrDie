1b709a7a304da16839f09d99334acb6ec42aef19
Jeremy <jeremyclark@live.ca>
2019-11-08 10:21:02 -0800

    Added Login and signup screen

---
+++ ParkFreeOrDie/CreateAccountViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CreateAccountViewController.swift
+//  ParkFreeOrDie
+//
+//  Created by jermey on 2019-11-08.
+//  Copyright © 2019 MerinClark Ltd. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
