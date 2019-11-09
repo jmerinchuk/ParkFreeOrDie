9f263002ba94d7e81049477f56e9e77cfc75458c
Jayce Merinchuk <merin_45@hotmail.com>
2019-11-08 22:38:34 -0500

    Added Manual VC, worked on GUI for Adding Parking

---
+++ ParkFreeOrDie/AccountVC.swift
@@ -0,0 +1,36 @@
+/*****************************************************************
+ * Project: ParkFreeOrDie
+ * Programmer: Jeremy Clark
+ * Programmer: Jayce Merinchuk
+ * File: AccountVC.swift
+ * Desccription:
+ *
+ * Sources:
+ *
+ *****************************************************************/
+
+// Imports
+import UIKit
+
+/*****************************************************************
+ * Class: AccountVC : UIViewController
+ * Description:
+*****************************************************************/
+class AccountVC : UIViewController {
+
+    // Class Variables
+    
+    
+    // Outlets
+    
+    
+    /*************************************************************
+     * Method: viewDidLoad()
+     * Description: Initial Loaded Function
+    *************************************************************/
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
---
+++ ParkFreeOrDie/ManualVC.swift
@@ -0,0 +1,36 @@
+/*****************************************************************
+ * Project: ParkFreeOrDie
+ * Programmer: Jeremy Clark
+ * Programmer: Jayce Merinchuk
+ * File: ManualVC.swift
+ * Desccription:
+ *
+ * Sources:
+ *
+ *****************************************************************/
+
+// Imports
+import UIKit
+
+/*****************************************************************
+ * Class: ManualVC : UIViewController
+ * Description:
+*****************************************************************/
+class ManualVC : UIViewController {
+
+    // Class Variables
+    
+    
+    // Outlets
+    
+    
+    /*************************************************************
+     * Method: viewDidLoad()
+     * Description: Initial Loaded Function
+    *************************************************************/
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
